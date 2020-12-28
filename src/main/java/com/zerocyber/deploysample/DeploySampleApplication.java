ef0ab09a76e66f5cc01155751969b8d9379964c0
zerocyber <zerocyber89@gmail.com>
2020-12-28 18:27:42 +0900

    auto create pid file after start app

--- src/main/java/com/zerocyber/deploysample/DeploySampleApplication.java
+++ src/main/java/com/zerocyber/deploysample/DeploySampleApplication.java
@@ -2,12 +2,18 @@ package com.zerocyber.deploysample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DeploySampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeploySampleApplication.class, args);
+        //SpringApplication.run(DeploySampleApplication.class, args); // 초기 설정
+    	
+    	// PID 파일을 생성하기 위한 설정(프로젝트 shutdown시 없어진다)
+    	SpringApplication springApplication = new SpringApplication(DeploySampleApplication.class);
+    	springApplication.addListeners(new ApplicationPidFileWriter());
+    	springApplication.run(args);
     }
 
 }
